b10a8e5a506e4db1171b89a6c8660fec8817f747
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2024-07-10 08:59:27 +0200

    Added docstring to url encoding func

--- stimline_idex/api/v1/modules/text_utils.py
+++ stimline_idex/api/v1/modules/text_utils.py
@@ -4,4 +4,10 @@ from urllib.parse import quote
 
 
 def url_encode_id(id: str) -> str:
+    """
+    Encode a non-UUID ID for use in a URL.
+
+    >>> id = "1/3-K-1"
+    >>> assert url_encode_id(id) == "1%2F3-K-1"
+    """
     return quote(id, safe="")
