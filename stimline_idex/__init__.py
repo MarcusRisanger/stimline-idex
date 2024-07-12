2b6ffc28a90cc5ae3bac81e2d33cc0f7809b3e01
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2024-07-12 14:02:34 +0200

    Chore/add docs (#3)
    * Move files to new dirs
    
    * Add defaults to optional return columns
    
    * Expose data models higher up in hierarchy
    
    * Clean up imports
    
    * Clean up imports
    
    * Added common logger for package
    
    * Added warnings for using top with soft deletes not returned
    
    * Generalized name for util file
    
    * Update utils with more .. utils
    
    * Remove querying logic from modules, get from utils
    
    * Update README.md
    
    * Update pyproject.toml
    
    * Update settings.json
    
    * Update README.md
    
    * Adjusted util to emit unused kwargs
    
    * Update README.md
    
    Update README.md

--- stimline_idex/__init__.py
+++ stimline_idex/__init__.py
@@ -1,4 +1,4 @@
-from .api.v1 import IDEXClient
-from .api.v1.auth import ApiKeyAuth, JwtAuth
+from .v1.api import IDEXClient
+from .v1.api.auth import ApiKeyAuth, JwtAuth
 
 __all__ = ["IDEXClient", "ApiKeyAuth", "JwtAuth"]
